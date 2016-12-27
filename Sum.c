13e6bb78926854bd7c070c252ffe90c9cde57155
Reshma MN <reshmanandagopal@gmail.com>
2016-12-27 17:39:13 +0530

    Create Sum.c

---
+++ Sum.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void) {
+    int num=556, n, digit, sum=0, count=0;
+    printf("Enter the numbers");
+    scanf("%d",&num);
+    if(num==0)
+    {
+        count=1;
+    }
+    else
+    {
+        if(num<0)
+        {
+            num*=(-1);
+        }
+        n=num;
+        while(n!=0)
+        {
+            digit=num%10;
+            n=num/10;
+            sum=sum+digit;
+            count++;
+        }
+    }
+    printf("Number of digits: %d\nSum of digits: %d\n",count,sum);
+	return 0;
+}
+
