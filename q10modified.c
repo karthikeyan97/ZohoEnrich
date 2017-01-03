20e0ab7e972b2e107e0f328ff072cfb9514afbbb
Reshma MN <reshmanandagopal@gmail.com>
2017-01-03 20:05:03 +0530

    Update q10modified.c

--- q10modified.c
+++ q10modified.c
@@ -1,3 +1,8 @@
+/*Given a string, write a function to check if it is a permutation of a palindrome. A palindrome is a word or phrase that is the same forwards and backwards. A permutation is a rearrangement of letters. The palindrome does not need to be limited to just dictionary words. 
+
+Input: tactcoa 
+Output: true (permutations: "tacocat", "atcocta", etc.)*/
+
 #include <stdio.h>
 #include<string.h>
 
