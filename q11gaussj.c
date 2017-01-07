d4cfeae6e8a7c09bf37dff493d879ae1305d5bf6
Reshma MN <reshmanandagopal@gmail.com>
2017-01-07 12:58:17 +0530

    Create q11gaussj.c

---
+++ q11gaussj.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i,j,k,m,det=1;
+    double x,a[5][5]={0};
+    printf("Enter the order:\n");
+    scanf("%d",&m);
+    printf("Enter the elements:\n");
+    
+    for(i=0;i<m;i++)
+    {
+        for(j=0;j<m;j++)
+        {
+            scanf("%lf",&a[i][j]);
+        }
+    }
+    
+    for(i=0;i<m;i++)
+    {
+        for(k=0;k<i;k++)
+        {
+            x=a[i][k]/a[k][k];
+            for(j=0;j<m;j++)
+            {
+                a[i][j]=a[i][j]-(x*a[k][j]);
+            }
+        }
+    }
+    
+    for(i=0;i<m;i++)
+    {
+        det=det*a[i][i];
+    }
+    
+    /*for(i=0;i<m;i++)
+    {
+        for(j=0;j<m;j++)
+        {
+            printf("%.2f\t",a[i][j]);
+        }
+        printf("\n");
+    }*/
+    
+    printf("\nThe determinant is %d\n",det);
+    
+	return 0;
+}
+
